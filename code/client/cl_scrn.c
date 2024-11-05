1421e83a3f203cdd4b6eee19f3120181c745c864
Noire.dev <141606820+noire-dev@users.noreply.github.com>
2024-11-05 17:54:42 +0300

    unicode

--- code/client/cl_scrn.c
+++ code/client/cl_scrn.c
@@ -133,11 +133,6 @@ static void SCR_DrawChar( int x, int y, float size, int ch ) {
 		return;
 	}
 
-	// Unicode Russian support
-	if (ch > 0x80) {
-		ch += 48;
-	}
-
 	ax = x;
 	ay = y;
 	aw = size;
@@ -175,11 +170,6 @@ void SCR_DrawSmallChar( int x, int y, int ch ) {
 
 	if ( y < -smallchar_height ) {
 		return;
-	}
-
-	// Unicode Russian support
-	if (ch > 0x80) {
-		ch += 48;
 	}
 
 	row = ch>>4;
