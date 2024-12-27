daa1a45df0ca685236a396f08f570a9f0e6e3150
Noire.dev <141606820+noire-dev@users.noreply.github.com>
2024-12-27 16:48:33 +0300

    Update q_shared.h

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -53,7 +53,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define DEMOEXT	"dm_"			// standard demo extension
 
 //===========================================================================
-#define USE_BSP_MODELS 1
+//#define USE_BSP_MODELS 1
 
 #ifdef _MSC_VER
 
